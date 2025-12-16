fc3bb850adf886635cf0e5217dbd6788fbb74b6f
Matias Llanos <llanosmatias97@gmail.com>
2025-12-16 00:41:22 -0300

    Cambio initializeApp a variables de entorno

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv';
 import admin from 'firebase-admin';
-import serviceAccount from '../../contratos-mdw-firebase-adminsdk-fbsvc-78f5c86ea2.json';
 
-// Inicializar Firebase Admin SDK
+dotenv.config();
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
 });
 
 export const auth = admin.auth();
